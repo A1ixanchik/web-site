a04cd7d142e4a3ba37ec66f8b211a3b23b95f709
Пользователь <kurmanbekovalihan84@gmail.com>
2022-12-10 01:30:48 +0600

    new-update

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
     const swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
-        loop:true,
     pagination: {
     el: ".swiper-pagination",
     clickable: true,
